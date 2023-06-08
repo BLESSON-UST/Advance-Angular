f37538bb28df2130efb634c9d27abbc057521b92
BLESSON-UST <Blesson.Abraham1@ust.com>
2023-06-08 18:07:03 +0530

    first commit

---
+++ angular-dev-portfolio-master/src/app/configuration.ts
@@ -0,0 +1,69 @@
+export const Configuration =
+{
+    profileUrl:"https://avatars.githubusercontent.com/u/127296571?v=4",
+    userName:"Blesson John Abraham",
+    userJob:"Software Developer",
+    userLocation:"Trivandrum",
+    userEmail:"blessonabraham97@gmail.com",
+    userDateOfBirth:"11/28/1997",
+    userAddress:"Kottayam/ Kerala",    
+    userPhone:"7510956597",
+    aboutMe:"Me Myself",
+};
+
+//Personal Informations
+
+export const Skills  = 
+[
+    "C","Java","Angular","Springboot","Python","Javascript"
+];
+
+//Max skills limit is 6 . If you add more than 6 skill , you may be disappointed.
+
+export const JobExperience  = 
+[
+    {company : "IBM / Serii" , rank : "Logistic Support Personal " , date:"07/2017 - 08/2016" , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    {company : "Albayrak Bilgisayar" , rank : "Technical Service Personal" , date:"07/2015 - 07/2015" , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+];
+
+//Max job experiences limit is 3 . If you add more than 3 skill , you may be disappointed.
+
+export const Education  = 
+[
+    { school : "Sivas Cumhuriyet University" , date:"09/2018 - Present" , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    { school : "Türk Telekom Vocational High School" , date:"09/2013 - 07/2017" , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+];
+
+//Max education limit is 3 . If you add more than 3 skill , you may be disappointed.
+
+export const Services  = 
+[
+    { serviceName : "Web Design" ,  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    { serviceName : "Desktop Development"  , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    { serviceName : "Software Architecture"  , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    { serviceName : "Social Media Management"  , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    { serviceName : "Logo Design" ,  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+];
+
+//Max services limit is 6 . If you add more than 6 skill , you may be disappointed.
+
+export const Testimonials  = 
+[
+    { name : "Person X" , company:"Facebook" ,  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam aliquet dui at ultricies. Donec fermentum lorem sit amet massa vestibulum, vel pellentesque lectus vestibulum." , review:85},
+    { name : "Person Y" , company:"Instagram" ,  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam aliquet dui at ultricies. Donec fermentum lorem sit amet massa vestibulum, vel pellentesque lectus vestibulum." , review:80},
+    { name : "Person Z" , company:"Uber" ,  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam aliquet dui at ultricies. Donec fermentum lorem sit amet massa vestibulum, vel pellentesque lectus vestibulum." , review:79},
+    { name : "Person W" , company:"Airbnb" ,  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam aliquet dui at ultricies. Donec fermentum lorem sit amet massa vestibulum, vel pellentesque lectus vestibulum." , review:99}
+
+];
+
+//Max testimonials limit is 4 . If you add more than 4 skill , you may be disappointed.
+
+export const SocialMedia =
+{
+    github:"github.com",
+    instagram:"instagram.com",
+    twitter:"twitter.com",
+    linkedin:"linkedin.com",
+    medium:"medium.com",
+    facebook:"facebook.com"
+}
